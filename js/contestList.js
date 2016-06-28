7b9ff2218927fbf3468895848b611e1b447e0bda
nitin kumar <knitin917@gmail.com>
2016-06-28 23:29:56 +0530

    ongoing feature added

--- js/contestList.js
+++ js/contestList.js
@@ -2,7 +2,7 @@ var date = new Date();
 var year = date.getFullYear();
 console.log(year);
 var month = date.getMonth();
-console.log("month" + (month+1));
+console.log(month);
 var day = date.getDate();
 console.log(day);
 var limit = 1000;
@@ -42,7 +42,12 @@ var tempDate = 28;
     var objectDate;
     var objectMonth;
     var objectYear;
+    var objectEndHour;
+    var objectEndMin;
     var found = false;
+    var objectStartDate;
+    var objectStartDateItem;
+
     function myFunction(data) {
       data = JSON.parse(data);
       data = data.objects;
@@ -53,31 +58,44 @@ var tempDate = 28;
         
         found = false;
         objectMonth = (data[i].end[5] + data[i].end[6]) ;
-       
+        objectEndHour = data[i].end[11] + data[i].end[12];
+        objectEndMin = data[i].end[14] + data[i].end[15];
         if(objectMonth<(month+1) )// those which have finished 
           {
-            console.log(objectMonth + "skipped");
             continue;
           }
 
         objectDate = (data[i].end[8] + data[i].end[9]);
 
-        if(objectDate<day && objectMonth==(month+1))
+        if((objectDate<day && objectMonth==(month+1) ) ||  ( objectDate<=day && objectMonth==(month+1) && objectEndHour <= date.getHours() )) // skip those which ended in the current month
         {
+          if(objectEndHour == date.getHours()  && objectEndMin>=date.getMinutes())
+          {
+            continue;
+          }
           continue;
         }
+
+
         var endDate = "";
+        var startDate = "";
         for(var j=0;j<18;j++)
         {
           if(j==10)
-              endDate+=" ";
-           if(j==11)
-            continue;
+              {
+                endDate+=" at ";
+                startDate +=" at ";
+                continue;
+              }
             endDate +=data[i].end[j]; 
+            startDate +=data[i].start[j];
         }
+
         var name = data[i].resource.name;
         var imgLink; 
           
+        objectStartDateItem = new Date(data[i].start[0] + data[i].start[1] + data[i].start[2] + data[i].start[3],data[i].start[5] + data[i].start[6], data[i].start[8] + data[i].start[9]);
+        objectStartDateItem.setHours(data[i].start[11] + data[i].start[12]);
          if(name==platforms[0])
          {
             imgLink = 0;
@@ -115,21 +133,23 @@ var tempDate = 28;
             imgLink = 8;
              found = true;
          }
-
-         if(found==true)
+         //console.log(objectStartDateItem);
+         if(found==true && (objectStartDateItem.getFullYear()<date.getFullYear() || (objectStartDateItem.getMonth() <= (date.getMonth() + 1) && objectStartDateItem.getDate()<= date.getDate() &&  objectStartDateItem.getHours()<=date.getHours())  ))
          {
 
+        //  console.log("day " + objectStartDateItem.getDate() + "year" + objectStartDateItem.getFullYear() + "month" + objectStartDateItem.getMonth() + " " + data[i].href) ;
+        console.log(objectEndHour + " " + objectEndMin + " " + date.getHours() + " " + date.getMinutes());
 
             $('#contestList').append(
               
-  '<div class="thecard"><div class="card-img"><img src="img/' + imgLink + '.png">'+
-    '</div><div class="card-caption"><span class="date">' + endDate + '</span>'+
+  '<p> ongoing</p><div class="thecard"><div class="card-img"><img src="img/' + imgLink + '.png">'+
+    '</div><div class="card-caption"><h1><span class="date">Starts on ' + startDate + '</span></h1>' + '<h1><span class="date"> ends on ' + endDate + '</span></h1>'+
       '<h1>' + data[i].event + '</h1></div>'+
     '<a href=" ' + data[i].href + '"class="' +resultCount + '"><div class="card-outmore"><h5>Go to challenge page</h5><i id="outmore-icon" class="fa fa-angle-right"></i>'+
     '</div></a></div>'
             );
             urls.push(data[i].href.toString());
-              //console.log(urls[resultCount] + "pushed");
+        
             resultCount++;
             
           }
@@ -146,13 +166,11 @@ function createEventListeners()
     var div = document.getElementById("contestList");
     for(var i=0;i<resultCount;i++)
     {
-      console.log("addEventListener");
-      var element = div.getElementsByClassName(i)[0];
+     var element = div.getElementsByClassName(i)[0];
       element.addEventListener("click",function(){
           //opentab(document.getElementById(i).href);
           var a = urls[this.className];
-          console.log(a.toString());
-          chrome.tabs.create({ url: a });
+           chrome.tabs.create({ url: a });
 
          // opentab(a);
       });
